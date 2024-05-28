b98a853eaeaa4d7d8fd949da8846aff532a0d743
jeongmin <jeongmin0521@naver.com>
2024-05-28 19:36:24 +0900

    jwt 로그인

--- src/main/java/com/example/backend/Controller/AccountController.java
+++ src/main/java/com/example/backend/Controller/AccountController.java
@@ -1,12 +1,21 @@
 package com.example.backend.Controller;
 
+import com.example.backend.service.JwtServiceImpl;
 import com.example.backend.Repository.MemberRepository;
 import com.example.backend.entity.Member;
+import com.example.backend.service.JwtService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
@@ -14,13 +23,38 @@ public class AccountController {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    JwtService jwtService;
+
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params, HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
         if (member != null) {
-            return member.getId();
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            res.addCookie(cookie);
+            return new ResponseEntity<>(id, HttpStatus.OK);
         }
 
-        return 0;
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/api/account/check")
+    public ResponseEntity check(@CookieValue(value = "token", required = false) String token) {
+        Claims claims = jwtService.getClaims(token);
+
+        if (claims != null) {
+            int id = Integer.parseInt(claims.get("id").toString());
+            return new ResponseEntity<>(id, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/backend/service/JwtService.java
@@ -0,0 +1,8 @@
+package com.example.backend.service;
+
+import io.jsonwebtoken.Claims;
+
+public interface JwtService {
+    public String getToken(String key, Object value);
+    Claims getClaims(String token);
+}
